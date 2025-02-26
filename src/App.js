3e356ed561428ce762fa24603a84f122bbf7b56d
princy nr <princynr@princys-MacBook-Pro.local>
2025-02-26 07:02:43 +0530

    star-Rating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import StarRating from './components/star-rating';
 import RandomColor from './components/random-color';
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
 
       {/* Random-color component */}
       <RandomColor/>
+      <StarRating noOfStars={10}/>
+      <StarRating/>
     </div>
   );
 }
